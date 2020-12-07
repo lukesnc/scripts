4a26f106f50d6b64c325977476817fbae89fbe19
lukesnc <lukeasimone@gmail.com>
2020-12-06 19:31:08 -0500

    cleaned up dis one

--- wsl-deb-setup.sh
+++ wsl-deb-setup.sh
@@ -10,9 +10,9 @@ sudo apt autoremove -y
 # Base
 sudo apt install -y build-essential curl git wget ssh
 # Langs
-sudo apt install -y python3 python3-pip python3-venv python2 ruby golang-go perl php gcc g++ default-jdk nodejs
+sudo apt install -y python3 python3-pip python3-venv python2 ruby-full golang-go perl php gcc g++ default-jdk nodejs
 # Extras
-sudo apt install -y libimage-exiftool-perl gdb vim ranger nmap netcat neofetch man-db
+sudo apt install -y libimage-exiftool-perl vim ranger nmap netcat neofetch man-db
 
 # Append bashrc
 echo >> ~/.bashrc
