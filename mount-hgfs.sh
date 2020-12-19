73e17a2da42672f60e979a8224441acb1a7f1fc9
lukesnc <35816797+lukesnc@users.noreply.github.com>
2020-12-19 01:54:27 -0500

    Update mount-hgfs.sh

--- mount-hgfs.sh
+++ mount-hgfs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# THIS SCRIPT SHOULD ONLY BE USED INSIDE A VIRTUAL MACHINE
+# THIS SCRIPT SHOULD ONLY BE USED INSIDE A VMWARE VIRTUAL MACHINE
 # Mount a folder from the host machine to /mnt/hgfs
-# Only works with VMWare
 
 sudo vmhgfs-fuse .host:/ /mnt/hgfs -o allow_other
