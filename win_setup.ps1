39dc05f0172cb0c3b384c1eac66cd2f4042a542a
lukesnc <35816797+lukesnc@users.noreply.github.com>
2021-05-02 19:30:44 -0400

    Update win_setup.ps1

--- win_setup.ps1
+++ win_setup.ps1
@@ -12,7 +12,7 @@ choco install 7zip googlechrome vlc jre8 vscode spotify steam reaper discord -y
 $response = Read-Host "Install extra software? [y/n]: "
 if ( $response -eq "y" ) 
 {
-    choco install obs pia qbittorrent vmware-workstation-player -y
+    choco install obs pia qbittorrent -y
 }
 
 echo "Done. Restart Windows."
