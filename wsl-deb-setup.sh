a6bd75a3e599dc39e8bcad92fbdedd917543a90c
Luke Simone <lukeasimone@gmail.com>
2021-10-05 14:23:26 -0400

    stripped back a bit

--- wsl-deb-setup.sh
+++ wsl-deb-setup.sh
@@ -9,10 +9,8 @@ sudo apt autoclean
 
 # Base
 sudo apt install -y build-essential unzip curl git wget ssh
-# Langs
-sudo apt install -y python3 python3-pip python3-venv gcc g++ default-jdk
 # Extras
-sudo apt install -y vim ranger nmap jq netcat neofetch man-db
+sudo apt install -y vim nmap jq netcat neofetch man-db
 
 # Append bashrc
 echo >> ~/.bashrc
