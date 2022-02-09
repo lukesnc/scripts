5ff0d2ba54d6848a4279648033afb55fc4ceea70
lukesnc <lukeasimone@gmail.com>
2022-02-08 23:56:55 -0500

    Removed unneeded code

--- wsl-deb-setup.sh
+++ wsl-deb-setup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-# Only use on Debian based distros
 
-# Updates
-sudo apt update
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean
-
-# Base
 sudo apt install -y build-essential unzip curl git wget ssh
-# Extras
-sudo apt install -y vim nmap jq netcat neofetch man-db
+sudo apt install -y vim nmap netcat man-db
 
 # Symlink to windows home dir
 ln -sf "/mnt/c/Users/$USER" ~/winhome
