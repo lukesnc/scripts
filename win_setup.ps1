e1af627773dbce7cdaaa6b09d36aaab19b278856
lukesnc <lukeasimone@gmail.com>
2022-03-23 01:19:50 -0400

    simplified

--- win_setup.ps1
+++ win_setup.ps1
@@ -1,14 +1,4 @@
 # Requirements: chocolatey
 
-choco upgrade all -y
-
-choco install 7zip googlechrome vlc jre8 vscode spotify steam discord -y
+choco install 7zip vlc qbittorrent gsudo -y
 choco install reaper reapack reaper-sws-extension -y
-
-$response = Read-Host "Install extra software? [y/n]: "
-if ( $response -eq "y" ) 
-{
-    choco install obs-studio pia qbittorrent -y
-}
-
-echo "Done. Restart Windows."
