c987c21635008ec87771694d03872cddd3fcb971
lukesnc <lukeasimone@gmail.com>
2022-04-05 14:21:26 -0400

    not using vmware anymore

--- mount-hgfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# THIS SCRIPT SHOULD ONLY BE USED INSIDE A VMWARE VIRTUAL MACHINE
-# Mount a folder from the host machine to /mnt/hgfs
-
-sudo vmhgfs-fuse .host:/ /mnt/hgfs -o allow_other
