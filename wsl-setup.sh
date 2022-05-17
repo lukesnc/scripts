3144240aa084319f5209bbe61d5fe3e76e64d35f
lukesnc <lukeasimone@gmail.com>
2022-05-17 04:13:33 -0400

    commented out deb packages, removed zsh install

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-sudo apt install -y build-essential unzip curl git wget ssh
+# Deb
+# sudo apt install -y build-essential unzip curl git wget ssh
+
 # Arch
-# sudo pacman -S base-devel unzip wget openssh git
+sudo pacman -S base-devel unzip wget openssh git
 
 # Symlink to windows home dir
 ln -sf "/mnt/c/Users/$USER" ~/winhome
-
-# zsh (Add the plugin to the list of plugins inside ~/.zshrc)
-sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
