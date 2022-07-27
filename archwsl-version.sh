099544d4927c07b5a18e9a2c637b9352be1dd809
lukesnc <lukeasimone@gmail.com>
2022-07-26 23:58:38 -0400

    removed archwsl-version

--- archwsl-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-V="22.3.18.0"
-echo "current: $V"
-echo -n " latest: "
-curl -s -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/yuk7/ArchWSL/releases | \
-  grep "tag_name" | \
-  head -1 | cut -c 18-26
