96bb2e3ba1c0e151571b46ab0138e5ac854dd4c3
lukesnc <lukeasimone@gmail.com>
2023-03-20 18:14:52 -0400

    simplified and added awesome vimrc install

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-echo "WSL Setup Script"
-
 cd ~
-sudo apt install -y build-essential unzip curl git wget ssh vim
-git clone https://github.com/lukesnc/scripts.git
+sudo apt install -y build-essential unzip curl git wget ssh vim python3 python3-pip
 git config --global core.editor vim
 
-cp scripts/.bash_aliases .
-echo "Installed .bash_aliases."
-
-cat scripts/ps1/arch.sh >> .bashrc
-echo "Set bash prompt to Arch."
+# Install awesome vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 ln -sf "/mnt/c/Users/$USER" ~/winhome
 echo "Created symlink to Windows home dir."
