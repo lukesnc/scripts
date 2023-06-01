37f288e0daae36c31a468688eab59b1df1bb9bdd
lukesnc <lukeasimone@gmail.com>
2023-06-01 01:10:22 -0400

    formatting/source credit

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # scripts
 
 ## WSL Script
+
 Install with curl:
+
 ```sh
 curl -o- https://raw.githubusercontent.com/lukesnc/scripts/master/wsl-setup.sh | bash
 ```
--- wsl-setup.sh
+++ wsl-setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo apt install -y build-essential unzip curl git wget ssh vim python3 python3-pip
+
 git config --global core.editor vim
 git config --global pull.rebase false
 
-# Install awesome vimrc
+# Source: https://github.com/amix/vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 ln -sf "/mnt/c/Users/$USER" ~/winhome
-echo "Created symlink to Windows home dir."
 
 echo "Done."
